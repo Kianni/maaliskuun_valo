0487cf6c825984fa686c4899e5c546c33f531318
Kirill <kirill@cyf.fi>
2021-03-03 15:00:15 +0200

    ex 6 & 7

---
+++ database/crudrepository.js
@@ -0,0 +1,45 @@
+const { response } = require("express");
+
+let counter = 0;
+let database = [
+  { id: counter++, latitude: 60, longitude: 70 },
+  { id: counter++, latitude: 40, longitude: 80 },
+];
+
+module.exports = {
+  findAll: () => {
+    return database;
+  },
+  findById: (id) => {
+    let temp = database.find((temp) => temp.id === id);
+    if (temp == undefined) {
+      response.statusCode = 404;
+      return "no such location";
+    } else {
+      response.statusCode = 200;
+      return temp;
+    }
+  },
+  deleteById: (id) => {
+    let newDB = database.filter((temp) => temp.id != id);
+
+    if (newDB.length == database.length) {
+      response.statusCode = 404;
+      return false;
+    } else {
+      response.statusCode = 204;
+      database = newDB;
+      return true;
+    }
+  },
+  postWithId: (body) => {
+    let newLocation = body;
+    newLocation.id = counter++;
+    database.push(newLocation);
+    if (database.find((lastLoc) => lastLoc.id === newLocation.id)) {
+      response.statusCode = 201;
+    } else {
+      response.statusCode = 500;
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const express = require("express");
+const database = require("./database/crudrepository.js");
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(express.static("public"));
-
-let customers = [
-  { id: 1, name: "Bob" },
-  { id: 2, name: "Prometheus" },
-];
-
-// HTTP GET http://localhost:8080/api/customers
-app.get("/api/customers", (req, res) => {
-  res.send(customers);
-});
-
-let database = [
-  { id: 1, latitude: 60, longitude: 70 },
-  { id: 2, latitude: 40, longitude: 80 },
-];
+app.use(express.json());
 
 app.get("/api/locations", (req, res) => {
-  res.render("index.html");
+  res.json(database.findAll());
+});
+
+app.get("/api/locations/:id([0-9]+)", (req, res) => {
+  let id = Number(req.params.id);
+  res.json(database.findById(id));
+});
+
+app.delete("/api/locations/:id([0-9]+)", (req, res) => {
+  let id = Number(req.params.id);
+
+  res.json(database.deleteById(id));
+});
+
+app.post("/api/locations/", (req, res) => {
+  let body = req.body;
+  res.json(database.postWithId(body));
 });
 
 app.listen(port, () => {
